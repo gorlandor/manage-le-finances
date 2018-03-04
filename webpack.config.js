f0d2832a94f62431a42e92bfa1d87af131783298
Giovanni Orlando <gorlandor@gmail.com>
2018-03-04 16:05:32 -0400

    Updates project dependencies and dev dependencies to latest version

--- webpack.config.js
+++ webpack.config.js
@@ -48,9 +48,9 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-                'process.env': {
-                    NODE_ENV: JSON.stringify('production')
-                }
-            })
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        })
     ]
 };
