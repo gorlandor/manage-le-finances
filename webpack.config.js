c22ab29289f49cc7b375d10f08c1f62dab8795f5
Giovanni Orlando <gorlandor@gmail.com>
2019-03-03 14:40:45 -0400

    Update project deps. Remove awesome-typescript-loader in favor of ts-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,68 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: {
-        'style.bundle': './src/style.entry.scss',
-        'app.bundle': './src/app.entry.tsx'
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: '[name].js'
-    },
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+  entry: {
+    "style.bundle": "./src/style.entry.scss",
+    "app.bundle": "./src/app.entry.tsx"
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "[name].js"
+  },
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            {
-                test: /\.(svg|png|jpg)$/,
-                use: 'url-loader'
-            },
-            {
-                test: /\.json$/,
-                use: 'json-loader'
-            },
-            {
-                test: /\.tsx?$/,
-                loader: "awesome-typescript-loader?silent=true"
-            },
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
-            }
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production')
-            }
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        })
-    ],
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendors.bundle",
-                    chunks: "all"
-                }
-            }
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg)$/,
+        use: "url-loader"
+      },
+      {
+        test: /\.json$/,
+        use: "json-loader"
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors.bundle",
+          chunks: "all"
         }
+      }
     }
+  }
 };
